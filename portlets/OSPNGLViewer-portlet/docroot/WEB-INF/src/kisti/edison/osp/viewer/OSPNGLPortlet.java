9a5faf618a70c053e0244b5e12cc67f6d0092bfd
K1714P1-PC\K1714P1 <yejinkwon@kisti.re.kr>
2018-03-08 14:01:09 +0900

    Create OSP NGL Viewer test 1. not included server's file explorer

---
+++ portlets/OSPNGLViewer-portlet/docroot/WEB-INF/src/kisti/edison/osp/viewer/OSPNGLPortlet.java
@@ -0,0 +1,11 @@
+package kisti.edison.osp.viewer;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class OSPNGLPortlet
+ */
+public class OSPNGLPortlet extends MVCPortlet {
+ 
+
+}
