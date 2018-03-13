c4976d17d636dc041b089c7edf41326cd88302aa
K1714P1-PC\K1714P1 <yejinkwon@kisti.re.kr>
2018-03-13 15:54:48 +0900

    change osp js files

---
+++ portlets/OSPJSMol-portlet/docroot/js/osp/osp-editor.js
@@ -0,0 +1,26 @@
+(function(window){
+	'use strict';
+
+	if( window.OSP ){
+		if( OSP.Editor )	return;
+	}
+	else
+		window.OSP = {};
+	
+	OSP.Editor = function( namespace, params ){
+		var ns = namespace;
+		var Editor = this;
+		var connector;
+		var dataType;
+		var currentData;
+		var mode;
+		var saveAction;
+		var fileExplorerDialogSection;
+		var fileExplorerId;
+		var eventEnable;
+		
+		
+		
+	};
+
+})(window);
