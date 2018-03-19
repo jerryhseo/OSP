1ca71420642380431881a0ce7921493a93f0c615
K1714P1-PC\K1714P1 <yejinkwon@kisti.re.kr>
2018-03-19 18:21:59 +0900

    update script files -plugin.js file update

--- portlets/OSPNGLViewer-portlet/docroot/js/plugins.js
+++ portlets/OSPNGLViewer-portlet/docroot/js/plugins.js
@@ -13,7 +13,6 @@ NGL.PluginRegistry = {
     },
 
     get: function( name ){
-    	console.log("test1");
         if( name in this.dict ){
             return this.dict[ name ];
         }else{
@@ -22,18 +21,15 @@ NGL.PluginRegistry = {
     },
 
     get names(){
-    	console.log("test2");
     	return Object.keys( this.dict );
     },
 
     get count(){
-    	console.log("test3");
         return this.names.length;
     },
 
     load: function( name, stage ){
         var path = this.get( name );
-        console.log("test plugin js");
         console.log(path);
         stage.loadFile( path, { name: name + " plugin" } );
     }
