8a881c72355116fe52c8d71fbe7e2a6b41ee09c4
kdhan <kdhan@gplusit.co.kr>
2018-12-11 15:22:05 +0900

    ScienceApp Layout Migration Dev Complete

--- portlets/edison-migration-2016-portlet/docroot/WEB-INF/html/applayout/migration.jsp
+++ portlets/edison-migration-2016-portlet/docroot/WEB-INF/html/applayout/migration.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="/common/init.jsp"%>
-<liferay-portlet:resourceURL var="searchPortURL" id="searchPort" copyCurrentRenderParameters="false" />
 <liferay-portlet:resourceURL var="updateLayoutURL" id="updateLayout" copyCurrentRenderParameters="false" />
 
 <style>
@@ -18,139 +17,34 @@
 
 <div class="h30"></div>
 
-<h3>APP LAYOUT Migration</h3>
-<h3>Default : flow-1-row-1-column</h3>
-<p>${totalCnt}</p>
+<h3>2018 APP LAYOUT Migration</h3>
+<h4>${totalCnt}</h4>
 수행건수 : <p id="changeLayoutCnt">0</p>
 NOT UPDATE 건수 : <p id="notUpdateCnt">0</p>
 에러건수 : <p id="changeErrorCnt">0</p>
-<input type="button" class="btn btn-default" value="실행" onclick="search();"/>
-
-<img id="loadingBox" src="${contextPath}/images/loading.gif" width="400" style="display: none;"/>
+<input type="button" class="btn btn-default" value="실행" onclick="<portlet:namespace/>execute();"/>
 
 <script type="text/javascript">
-	function search(){
+	function <portlet:namespace/>execute(){
 		bStart();
 		setTimeout(function(){
-			var dataList = JSON.parse('${swList}');
-			for(var i=0;i<dataList['sw'].length;i++){
-				var scienceAppId = dataList['sw'][i];
-				ajaxSearchApp(scienceAppId);
-			}
+			<portlet:namespace/>migrationExecute();
 			bEnd();
 		}, 2000);
 	}
 	
-	function ajaxSearchApp(scienceAppId){
-		console.log(scienceAppId);
-		var dataForm = {
-				"<portlet:namespace/>scienceAppId" : scienceAppId
-		};
-		
-		jQuery.ajax({
-			type: "POST",
-			url: "<%=searchPortURL%>",
-			async : false,
-			data : dataForm,
-			success: function(msg) {
-				var updateStatus = msg.updateStatus;
-				if(updateStatus){
-					var dataMap = msg.data;
-					var scienceApp = new OSP.ScienceApp();
-					if(typeof dataMap.inputPorts != 'undefined'){
-						scienceApp.deserializeInputPorts(JSON.parse(dataMap.inputPorts));
-					}
-					
-					if(typeof dataMap.outputPorts != 'undefined'){
-						scienceApp.deserializeOutputPorts(JSON.parse(dataMap.outputPorts));
-					}
-					
-					if(typeof dataMap.logPorts != 'undefined'){
-						scienceApp.deserializeLogPorts(JSON.parse(dataMap.logPorts));
-					}
-					
-					
-					
-					var Layout = new OSP.Layout();
-					Layout.templateId("flow-1-row-1-column");
-					
-					//System Default Portlet Set
-					Layout.addPortlet('column-1','SimulationDashboard_WAR_edisonsimulationportlet',true);
-					Layout.addPortlet('column-2','SimulationBreadcrumb_WAR_OSPWorkbenchportlet',true);
-					Layout.addPortlet('column-3','ScienceAppPort_WAR_OSPWorkbenchportlet',true);
-					
-					
-					var inputPortArray = scienceApp.inputPortsArray();
-					var editorFirst = true;
-					for(var i=0; i<inputPortArray.length;i++){
-						var data = inputPortArray[i];
-						if(data[OSP.Constants.DEFAULT_EDITOR]==0){
-							alert(Liferay.Language.get('edison-science-appstore-port-no-default-error-msg',[''+data[OSP.Constants.NAME]+'','Editor']));
-							return false;
-						}
-						Layout.addPortlet('column-4',data[OSP.Constants.DEFAULT_EDITOR],editorFirst,data[OSP.Constants.NAME])
-						editorFirst = false;
-					}
-					
-					
-					var logPortArray = scienceApp.logPortsArray();
-					if(logPortArray.length>0){
-						var logFirst = true;
-						for(var i=0; i<logPortArray.length;i++){
-							var data = logPortArray[i];
-							if(data[OSP.Constants.DEFAULT_ANALYZER]==0){
-								alert(Liferay.Language.get('edison-science-appstore-port-no-default-error-msg',[''+data[OSP.Constants.NAME]+'','Analyzer']));
-								return false;
-							}
-							
-							Layout.addPortlet('column-5',data[OSP.Constants.DEFAULT_ANALYZER],logFirst,data[OSP.Constants.NAME]);
-							logFirst = false;
-						}
-					}
-					
-					
-					var outputPortArray = scienceApp.outputPortsArray();
-					if(outputPortArray.length>0){
-						var outPutFirst = true;
-						for(var i=0; i<outputPortArray.length;i++){
-							var data = outputPortArray[i];
-							if(data[OSP.Constants.DEFAULT_ANALYZER]==0){
-								alert(Liferay.Language.get('edison-science-appstore-port-no-default-error-msg',[''+data[OSP.Constants.NAME]+'','Analyzer']));
-								return false;
-							}
-							Layout.addPortlet('column-6',data[OSP.Constants.DEFAULT_ANALYZER],outPutFirst,data[OSP.Constants.NAME]);
-							outPutFirst = false;
-						}
-					}
-	  				console.log(JSON.stringify(Layout,null,4));
-	 				update(scienceAppId,Layout);
-				}else{
-					var num = $("#notUpdateCnt").html();
-					$("#notUpdateCnt").html(Number(num)+1);
-				}
-			},error:function(msg,e){
-			}
-		});
-	}
-	
-	
-	function update(scienceAppId,layoutJSON){
-		var dataForm = {
-				"<portlet:namespace/>scienceAppId" : scienceAppId,
-				"<portlet:namespace/>layout" : JSON.stringify(layoutJSON),
-				"<portlet:namespace/>templetId" : 'flow-1-row-1-column'
-		};
+	function <portlet:namespace/>migrationExecute(){
 		jQuery.ajax({
 			type: "POST",
 			url: "<%=updateLayoutURL%>",
 			async : false,
-			data : dataForm,
+			dataType: 'json',
 			success: function(msg) {
-				var num = $("#changeLayoutCnt").html();
-				$("#changeLayoutCnt").html(Number(num)+1);
-			},error:function(msg,e){ 
-				var num = $("#changeErrorCnt").html();
-				$("#changeErrorCnt").html(Number(num)+1);
+				$("#changeLayoutCnt").html(msg.successCnt);
+				$("#notUpdateCnt").html(msg.notUpdateCnt);
+				$("#changeErrorCnt").html(msg.errorCnt);
+			},error:function(msg,e){
+				alert("ERROR");
 			}
 		});
 	}
--- portlets/edison-migration-2016-portlet/docroot/WEB-INF/src/org/kisti/edison/migration/portlet/applayout/ApplayoutController.java
+++ portlets/edison-migration-2016-portlet/docroot/WEB-INF/src/org/kisti/edison/migration/portlet/applayout/ApplayoutController.java
@@ -2,9 +2,9 @@ package org.kisti.edison.migration.portlet.applayout;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.RenderRequest;
@@ -13,12 +13,8 @@ import javax.portlet.ResourceResponse;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kisti.edison.science.model.ScienceApp;
-import org.kisti.edison.science.service.ScienceAppInputPortsLocalServiceUtil;
 import org.kisti.edison.science.service.ScienceAppLocalServiceUtil;
-import org.kisti.edison.science.service.ScienceAppLogPortsLocalServiceUtil;
-import org.kisti.edison.science.service.ScienceAppOutputPortsLocalServiceUtil;
-import org.kisti.edison.util.CustomUtil;
-import org.kisti.edison.util.RequestUtil;
+import org.kisti.edison.science.service.constants.ScienceAppConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +24,11 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 /*
  * APP LAYOUT 정보 UPDATE
@@ -43,91 +41,272 @@ public class ApplayoutController {
 	
     @RequestMapping//default
 	public String view(RenderRequest request, ModelMap model) throws PortalException, SystemException{
-        List<ScienceApp> appList = ScienceAppLocalServiceUtil.retrieveListByTemplateId("2-6-column-i");
-        int totalCnt = appList.size();
-		
-		List<String> swAppIds = new ArrayList<String>();
-		for(ScienceApp scienceApp : appList){
-			swAppIds.add(String.valueOf(scienceApp.getScienceAppId()));
-		}
-		
-		JSONObject sw = new JSONObject();
-		sw.put("sw", swAppIds);
-		
-		model.addAttribute("totalCnt", totalCnt);
-		model.addAttribute("swList", sw.toString());
-		
+    	ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+    	
+    	
+    	long groupId = PortalUtil.getScopeGroupId(request);
+    	Locale locale = themeDisplay.getLocale();
+    	long authorId = 0;
+    	String[] appTypes = new String[]{"Solver"};
+    	String[] editorTypes = null;
+    	
+    	Map<String,Object>searchMap = new HashMap<String,Object>();
+    	String status = "";
+    	int begin = 0;
+    	int end = 0;
+    	boolean lanuageSearch = false;
+    	
+    	int totalCnt = ScienceAppLocalServiceUtil.countListScienceApp(groupId, locale, authorId, appTypes, editorTypes, searchMap, status, lanuageSearch);
+    	
+    	model.addAttribute("totalCnt", totalCnt);
 		return "migration";
 	}
 	
 	
-	@ResourceMapping(value="searchPort")
-	public void searchPort(ResourceRequest request, ResourceResponse response) throws Exception{
-		Map params = RequestUtil.getParameterMap(request);
-		long scienceAppId = GetterUtil.getLong(params.get("scienceAppId"),0);
-		if(scienceAppId!=0){
-			long inputCnt = ScienceAppInputPortsLocalServiceUtil.getScienceAppInputPortsesCount(scienceAppId);
-			long outputCnt = ScienceAppOutputPortsLocalServiceUtil.getScienceAppOutputPortsesCount(scienceAppId);
-			long logCnt = ScienceAppLogPortsLocalServiceUtil.getScienceAppLogPortsesCount(scienceAppId);
-			Map<String,Object> data = new HashMap<String,Object>();
-			
-			boolean updateStatus = false;
-			//port 조회
-			String inputPorts = "";
-			if(inputCnt!=0){
-				inputPorts = ScienceAppInputPortsLocalServiceUtil.getInputPortsJsonString(scienceAppId);
-				data.put("inputPorts", inputPorts);
-				updateStatus = true;
+	@ResourceMapping(value="updateLayout")
+	public void updateLayout(ResourceRequest request, ResourceResponse response) throws IOException{
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+    	
+		int successCnt = 0;
+		int errorCnt = 0;
+		int notUpdateCnt = 0;
+		
+		try{
+			List<ScienceApp> appList = ScienceAppLocalServiceUtil.getAll();
+			for(ScienceApp scienceApp: appList){
+				if(scienceApp.getAppType().equals(ScienceAppConstants.APP_TYPE_SOLVER)){
+					if(scienceApp.getLayout().equals("")){
+						notUpdateCnt++;
+					}else{
+						JSONObject appLayoutJson = JSONObject.fromObject(scienceApp.getLayout());
+						if(appLayoutJson.has("isStepLayout_")){
+							notUpdateCnt++;
+						}else{
+							String templateId = appLayoutJson.get("templateId_").toString();
+							if(templateId.indexOf("flow")>-1){
+								//FLOW UPDATE
+								scienceApp.setLayout(this.flowLayoutUpdate(appLayoutJson,templateId));
+								scienceApp.setIsStepLayout(true);
+							}else{
+								//ONECOLUMN UPDATE
+								scienceApp.setLayout(this.oneLayoutUpdate(appLayoutJson,templateId));
+								scienceApp.setIsStepLayout(false);
+							}
+							
+							if(!this.updateScienceApp(scienceApp)){
+								errorCnt++;
+							}else{
+								successCnt++;
+							}
+						}
+					}
+					
+				}
 			}
-			
-			String outputPorts = "";
-			if(outputCnt!=0){
-				outputPorts = ScienceAppOutputPortsLocalServiceUtil.getOutputPortsJsonString(scienceAppId);
-				data.put("outputPorts", outputPorts);
-				updateStatus = true;
-			}
-			
-			String logPorts = "";
-			if(logCnt!=0){
-				logPorts = ScienceAppLocalServiceUtil.getScienceAppLogPorts(scienceAppId);
-				data.put("logPorts", logPorts);
-				updateStatus = true;
-			}
-			
 			
 			JSONObject json = new JSONObject();
-			json.put("data", data);
-			json.put("updateStatus", updateStatus);
+			json.put("successCnt", successCnt);
+			json.put("errorCnt", errorCnt);
+			json.put("notUpdateCnt", notUpdateCnt);
 			
 			response.setContentType("application/json; charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			out.write(json.toString());
-		}
-	}
-	
-	@ResourceMapping(value="updateLayout")
-	public void updateLayout(ResourceRequest request, ResourceResponse response) throws IOException{
-		Map params = RequestUtil.getParameterMap(request);
-		long scienceAppId = GetterUtil.getLong(params.get("scienceAppId"),0);
-		
-		try{
-			String layout = CustomUtil.strNull(params.get("layout"));
-			String templetId = CustomUtil.strNull(params.get("templetId"));
+		}catch(SystemException e){
 			
-			ScienceApp scienceApp = ScienceAppLocalServiceUtil.getScienceApp(scienceAppId);
-			scienceApp.setLayout(layout);
-			scienceApp.setTempletId(templetId);
+		}
+	}
+	
+	private Boolean updateScienceApp(ScienceApp scienceApp){
+		try {
 			ScienceAppLocalServiceUtil.updateScienceApp(scienceApp);
-			System.out.println("SUCCESS--->"+scienceAppId);
-		}catch(Exception e){
+			return true;
+		} catch (Exception e) {
+			System.out.print("ERROR APPID==============>"+scienceApp.getScienceAppId());
+			System.out.println("__APP_NAME==>"+scienceApp.getName()+"====APP_Version===>"+scienceApp.getVersion());
 			e.printStackTrace();
-			System.out.println("EXCEPTION--->"+scienceAppId);
-			handleRuntimeException(e, PortalUtil.getHttpServletResponse(response), "수정오류");
+			return false;
+		}
+	}
+	
+	/*FLOW LAYOUT UPDATE*/
+	private String flowLayoutUpdate(JSONObject appLayoutJson, String templateId){
+		System.out.println("===========================FLOW==PRE============================");
+		System.out.println(appLayoutJson.toString());
+		
+		JSONObject updateLayoutJson = new JSONObject();
+		updateLayoutJson.put("isStepLayout_", true);
+		
+		
+		JSONArray preColumns = appLayoutJson.getJSONArray("columns_");
+		JSONArray arrayKeys = new JSONArray();
+		boolean isOutPutLayout = false;
+		for(int i=0;i<preColumns.size();i++){
+			JSONObject preColumn = preColumns.getJSONObject(i);
+			if(preColumn.getString("id_").equals("column-1")){
+				//SYSTEM
+				arrayKeys.add("SYSTEM");
+				updateLayoutJson.put("SYSTEM", this.preLayoutFlowColumnsUpdate(preColumn, "SYSTEM"));
+			}else if(preColumn.getString("id_").equals("column-4")){
+				//INPUT
+				arrayKeys.add("INPUT");
+				updateLayoutJson.put("INPUT", this.preLayoutFlowColumnsUpdate(preColumn, "INPUT"));
+			}else if(preColumn.getString("id_").equals("column-5")){
+				//LOG
+				arrayKeys.add("LOG");
+				updateLayoutJson.put("LOG", this.preLayoutFlowColumnsUpdate(preColumn, "LOG"));
+			}else if(preColumn.getString("id_").equals("column-6")){
+				isOutPutLayout = true;
+			}
 		}
 		
+		if(isOutPutLayout){
+			//OUPUT
+			arrayKeys.add("OUTPUT");
+			updateLayoutJson.put("OUTPUT", this.preFlowLayoutOutput(appLayoutJson, templateId));
+		}
+		
+		updateLayoutJson.put("arrayKeys_", arrayKeys);
+		System.out.println("===========================FLOW==UPDATE============================");
+		System.out.println(updateLayoutJson.toString());
+		
+		return updateLayoutJson.toString();
 	}
 	
 	
+	private JSONObject preFlowLayoutOutput(JSONObject appLayoutJson,String templateId){
+		JSONObject updateLayoutJson = new JSONObject();
+		
+		String newTemplateId = templateId.replaceAll("flow-", "");
+		updateLayoutJson.put("templateId_", newTemplateId);
+		updateLayoutJson.put("layoutName_", "OUTPUT");
+		
+		JSONArray updateColumnsJson = new JSONArray();
+		JSONArray preColumns = appLayoutJson.getJSONArray("columns_");
+		for(int i=0;i<preColumns.size();i++){
+			JSONObject preColumn = preColumns.getJSONObject(i);
+			String preColumnId = preColumn.getString("id_");
+			int preColumnIdIndex = Integer.parseInt(preColumnId.replaceAll("[^0-9]", ""));
+			if(preColumnIdIndex>5){
+				String newColumnId = "column-"+(preColumnIdIndex-2);
+				updateColumnsJson.add(this.columnUpldate(preColumn, "OUTPUT",newColumnId));
+			}
+		}
+		
+		updateLayoutJson.put("columns_", updateColumnsJson);
+		return updateLayoutJson;
+	}
+	
+	private JSONObject preLayoutFlowColumnsUpdate(JSONObject preColumn, String layoutKey){
+		JSONObject updateLayoutJson = new JSONObject();
+		JSONArray updateColumnsJson = new JSONArray();
+		if(layoutKey.equals("SYSTEM")){
+			updateColumnsJson.add("{\"id_\":\"column-1\",\"portlets_\":[{\"instanceId_\":\"SimulationDashboard_WAR_edisonsimulationportlet_INSTANCE_SYSTEM\"}],\"currentPortlet_\":\"SimulationDashboard_WAR_edisonsimulationportlet_INSTANCE_SYSTEM\"}");
+			updateColumnsJson.add("{\"id_\":\"column-2\",\"portlets_\":[{\"instanceId_\":\"SimulationJobController_WAR_edisonsimulationportlet_INSTANCE_SYSTEM\"}],\"currentPortlet_\":\"SimulationJobController_WAR_edisonsimulationportlet_INSTANCE_SYSTEM\"}");
+		}else if(layoutKey.equals("INPUT")){
+			updateColumnsJson.add(this.columnUpldate(preColumn, "INPUT", "column-4"));
+			updateLayoutJson.put("templateId_", "1-row-1-column");
+		}else if(layoutKey.equals("LOG")){
+			updateColumnsJson.add(this.columnUpldate(preColumn, "LOG", "column-4"));
+			updateLayoutJson.put("templateId_", "1-row-1-column");
+		}
+		
+		updateLayoutJson.put("columns_", updateColumnsJson);
+		updateLayoutJson.put("layoutName_", layoutKey);
+		return updateLayoutJson;
+	}
+	
+	/*NO-FLOW LAYOUT UPDATE*/
+	private String oneLayoutUpdate(JSONObject appLayoutJson, String templateId){
+		System.out.println("=============================PRE============================");
+		System.out.println(appLayoutJson.toString());
+		
+		JSONObject updateLayoutJson = new JSONObject();
+		updateLayoutJson.put("isStepLayout_", false);
+		updateLayoutJson.put("arrayKeys_", new String[]{"LAYOUT"});
+		
+		JSONObject layoutJson = new JSONObject();
+		layoutJson.put("templateId_", templateId);
+		layoutJson.put("layoutName_", "LAYOUT");
+
+		
+		//layout AREA
+		JSONArray uplodateColumns = this.preLayoutOneColumnsUpdate(appLayoutJson);
+		layoutJson.put("columns_", uplodateColumns);
+		updateLayoutJson.put("LAYOUT", layoutJson);
+		
+		System.out.println("=============================UPDATE============================");
+		System.out.println(updateLayoutJson.toString());
+		
+		return updateLayoutJson.toString();
+	}
+	
+	private JSONArray preLayoutOneColumnsUpdate(JSONObject appLayoutJson){
+		JSONArray updateColumnsJson = new JSONArray();
+		updateColumnsJson.add("{\"id_\":\"column-1\",\"portlets_\":[{\"instanceId_\":\"SimulationDashboard_WAR_edisonsimulationportlet_INSTANCE_LAYOUT\"}],\"currentPortlet_\":\"SimulationDashboard_WAR_edisonsimulationportlet_INSTANCE_LAYOUT\"}");
+		updateColumnsJson.add("{\"id_\":\"column-2\",\"portlets_\":[{\"instanceId_\":\"SimulationJobController_WAR_edisonsimulationportlet_INSTANCE_LAYOUT\"}],\"currentPortlet_\":\"SimulationJobController_WAR_edisonsimulationportlet_INSTANCE_LAYOUT\"}");
+		
+		
+		
+		JSONArray preColumns = appLayoutJson.getJSONArray("columns_");
+		for(int i=0;i<preColumns.size();i++){
+			JSONObject preColumn = preColumns.getJSONObject(i);
+			String preColumnId = preColumn.getString("id_");
+			if(preColumnId.equals("column-1")||preColumnId.equals("column-2")||preColumnId.equals("column-3")){
+				
+			}else{
+				updateColumnsJson.add(this.columnUpldate(preColumn, "LAYOUT", preColumnId));
+			}
+		}
+		
+		return updateColumnsJson;
+	}
+	
+	private JSONObject columnUpldate(JSONObject preColumn,String layoutKey,String preColumnId){
+		int preColumntIndex = Integer.parseInt(preColumnId.replaceAll("[^0-9]", ""))-1;
+		String newColumnId = "column-"+preColumntIndex;
+		
+		JSONObject updateColumn = new JSONObject();
+		updateColumn.put("id_", newColumnId);
+		
+		if(preColumn.has("currentPortlet_")){
+			updateColumn.put("currentPortlet_", this.portletIdUpdate(preColumn.getString("currentPortlet_"),layoutKey));
+			
+			JSONArray preColumnPortlets = preColumn.getJSONArray("portlets_");
+			JSONArray newColumnPortlets = new JSONArray();
+			
+			for(int i=0;i<preColumnPortlets.size();i++){
+				JSONObject preColumnPortlet = preColumnPortlets.getJSONObject(i);
+				JSONObject newColumnPortlet = new JSONObject();
+				
+				newColumnPortlet.put("instanceId_", this.portletIdUpdate(preColumnPortlet.getString("instanceId_"),layoutKey));
+				if(preColumnPortlet.has("portName_")){
+					newColumnPortlet.put("portName_", preColumnPortlet.getString("portName_"));
+				}
+				
+				newColumnPortlets.add(newColumnPortlet);
+			}
+			
+			updateColumn.put("portlets_", newColumnPortlets);
+		}else{
+			updateColumn.put("portlets_", preColumn.getJSONArray("portlets_"));
+		}
+		
+		return updateColumn;
+	}
+	
+	private String portletIdUpdate(String prePortletId, String layoutKey){
+		String newPortletId = "";
+		if(prePortletId.indexOf("INSTANCE")>-1){
+			String prePortletIndex = prePortletId.replaceAll("[^0-9]", "");
+			String preShortPortletId = prePortletId.substring(0,prePortletId.indexOf("INSTANCE")-1);
+			newPortletId = preShortPortletId+"_INSTANCE_"+layoutKey+"_"+prePortletIndex;
+		}else{
+			newPortletId = prePortletId+"_INSTANCE_"+layoutKey;
+		}
+		
+		return newPortletId;
+	}
 	
 	public static void handleRuntimeException(Exception ex, HttpServletResponse response,String message) throws IOException {
 		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
