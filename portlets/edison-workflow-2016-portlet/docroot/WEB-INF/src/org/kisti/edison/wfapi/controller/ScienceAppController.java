7b21a178c4fc74feebf3336ab3a4473abb54bf29
kdhan <kdhan@gplusit.co.kr>
2018-12-26 15:42:31 +0900

    Workflow inputport editor list add

--- portlets/edison-workflow-2016-portlet/docroot/WEB-INF/src/org/kisti/edison/wfapi/controller/ScienceAppController.java
+++ portlets/edison-workflow-2016-portlet/docroot/WEB-INF/src/org/kisti/edison/wfapi/controller/ScienceAppController.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.kisti.edison.science.model.ScienceApp;
 import org.kisti.edison.science.service.PortTypeAnalyzerLinkLocalServiceUtil;
 import org.kisti.edison.science.service.ScienceAppInputPortsLocalServiceUtil;
 import org.kisti.edison.science.service.ScienceAppLocalServiceUtil;
@@ -24,10 +25,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.kisti.osp.icecap.model.DataType;
+import com.kisti.osp.icecap.service.DataTypeEditorLocalServiceUtil;
 import com.kisti.osp.icecap.service.DataTypeLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -118,20 +121,31 @@ public class ScienceAppController{
     	  for (Map.Entry<String, JsonElement> entry: entries) {
     		  JsonObject port = entry.getValue().getAsJsonObject();
     		  port.addProperty("wfSample_", false);
+    		  JsonObject dataType = port.get("dataType_").getAsJsonObject();
+			  String dtName = dataType.get("name").getAsString();
+			  String dtVersion = dataType.get("version").getAsString();
+			  DataType dataTypeModel = DataTypeLocalServiceUtil.findDataTypeObject(dtName, dtVersion);
+			  
     		  if(!port.has("sample_")){
-    			  JsonObject dataType = port.get("dataType_").getAsJsonObject();
-    			  String dtName = dataType.get("name").getAsString();
-    			  String dtVersion = dataType.get("version").getAsString();;
-    			  DataType dataTypeModel = DataTypeLocalServiceUtil.findDataTypeObject(dtName, dtVersion);
-    			  
     			  JsonObject sample = new JsonObject();
     			  sample.addProperty("id_", dataTypeModel.getSamplePath());
     			  sample.addProperty("type_", "dlEntryId_");
     			  sample.addProperty("relative_", true);
     			  port.add("sample_", sample);
     		  }
+    		  
+    		  
+    		  JsonArray editors = new JsonArray();
+    		  List<Map<String, Object>> editorList = DataTypeEditorLocalServiceUtil.retrieveDataTypeEditorList(dataTypeModel.getTypeId());
+    		  for(Map<String,Object> editor : editorList){
+    			  ScienceApp scienceApp = ScienceAppLocalServiceUtil.getScienceApp(GetterUtil.getLong(editor.get("editorId")));
+    			  JsonObject editorObject = new JsonObject();
+    			  editorObject.addProperty("name", scienceApp.getName());
+    			  editorObject.addProperty("value", scienceApp.getExeFileName());
+    			  editors.add(editorObject);
+    		  }
+    		  port.add("editors_", editors);
     	  }
-    	  
         return ports.toString();
       }else{
         return "{}";
