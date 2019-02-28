d6958df8e7389d83ccc52e12f26af40d7ef9e350
javantea <yjpakr@gplusit.co.kr>
2019-02-28 17:19:37 +0900

    fin responsive search

--- portlets/edison-search-2017-portlet/docroot/WEB-INF/service/org/kisti/edison/search/service/service/SearchLocalServiceUtil.java
+++ portlets/edison-search-2017-portlet/docroot/WEB-INF/service/org/kisti/edison/search/service/service/SearchLocalServiceUtil.java
@@ -291,8 +291,7 @@ public class SearchLocalServiceUtil {
 	}
 
 	/**
-	* 사이트 분야 카테고리 조회
-	*/
+	* 占싼딆뵠占쏙옙�브쑴鍮�燁삳똾�믤�醫듼봺 鈺곌퀬��     */
 	public static java.util.List<com.liferay.portlet.asset.model.AssetCategory> getLv1Categories(
 		long companyGroupId, long groupId, java.util.Locale locale)
 		throws com.liferay.portal.kernel.exception.PortalException,
@@ -301,7 +300,7 @@ public class SearchLocalServiceUtil {
 	}
 
 	/**
-	* 카테고리 JSTree 데이터 조회
+	* 燁삳똾�믤�醫듼봺 JSTree 占쎄퀣�좑옙占썼�怨좎돳
 	*/
 	public static java.lang.String getCategoriesJsonString(
 		long companyGroupId, long groupId, java.util.Locale locale)
