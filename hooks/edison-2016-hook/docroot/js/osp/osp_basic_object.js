0f4ca3b403ccff296b2fba8ea5c8387287e5a30b
DELL <DELL@112.160.83.45>
2019-02-28 23:01:59 +0900

    Home 2019.2.28: Change generating file explorer instance ID

--- hooks/edison-2016-hook/docroot/js/osp/osp_basic_object.js
+++ hooks/edison-2016-hook/docroot/js/osp/osp_basic_object.js
@@ -785,6 +785,18 @@
                 slashIndex = fileName.lastIndexOf('/');
                  
 			return fileName.slice(slashIndex+1);
+        },
+        randomString: function( length, code ){
+            var mask = '';
+            if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+            if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            if (chars.indexOf('1') > -1) mask += '0123456789';
+            if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+            var result = '';
+            for (var i = length; i > 0; --i){
+                result += mask[Math.floor(Math.random() * mask.length)];
+            } 
+            return result;
         }
     }; // End of OSP.Util
 
